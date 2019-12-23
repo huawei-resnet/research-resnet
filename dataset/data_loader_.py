37c56533fe6cc77bce490c03bb277df07c8df795
Shampooh <agent.serega@yandex.ru>
2019-12-23 14:34:39 +0300

    Add CIFAR100 preproccessing support

--- dataset/data_loader_.py
+++ dataset/data_loader_.py
@@ -51,3 +51,53 @@ class CIFAR10Data(object):
             num_workers=2, shuffle=False
         )
         return test_loader
+
+class CIFAR100Data(object):
+    def __init__(self, train_split=0.9):
+        train_transform = transforms.Compose([
+            transforms.RandomCrop(size=(32, 32), padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean, std=std)
+        ])
+        val_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean, std=std)
+        ])
+        test_transform = val_transform
+        train_dataset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True,
+                                                     transform=train_transform)
+        val_dataset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True,
+                                                   transform=val_transform)
+        test_dataset = torchvision.datasets.CIFAR100(root='./data', train=False, download=True,
+                                                    transform=test_transform)
+
+        num_train = len(train_dataset)
+        indices = list(range(num_train))
+        split = int(num_train * train_split)
+        train_idx, val_idx = indices[:split], indices[split:]
+        self.X_train = train_dataset.train_data[train_idx]
+        self.train_dataset = torch.utils.data.Subset(train_dataset, train_idx)
+        self.val_dataset = torch.utils.data.Subset(val_dataset, val_idx)
+        self.test_dataset = test_dataset
+
+    def get_train_loader(self, batch_size=128):
+        train_loader = torch.utils.data.DataLoader(
+            self.train_dataset, batch_size=batch_size,
+            num_workers=2, shuffle=True
+        )
+        return train_loader
+
+    def get_val_loader(self, batch_size=128):
+        val_loader = torch.utils.data.DataLoader(
+            self.val_dataset, batch_size=batch_size,
+            num_workers=2, shuffle=False
+        )
+        return val_loader
+
+    def get_test_loader(self, batch_size=128):
+        test_loader = torch.utils.data.DataLoader(
+            self.test_dataset, batch_size=batch_size,
+            num_workers=2, shuffle=False
+        )
+        return test_loader
